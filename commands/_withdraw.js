5c87d2c8ee7aefdb4047f7564c235a7525961542
Krusas <wtipperbot@gmail.com>
2024-08-24 06:35:12 +0800

    Update _withdraw.js

--- commands/_withdraw.js
+++ commands/_withdraw.js
@@ -10,23 +10,25 @@
   group: 
 CMD*/
 
-if (!params && content) {
+if (content) {
   var json = JSON.parse(content)
-  if (json.message.status == "progressing") {
+  if (json.message) {
     Bot.sendMessage("*Withdrawal has been created 1-5 minutes to get txhash*")
   } else {
     Bot.sendMessage(inspect(content))
   }
   return
 }
-var prms = params.split(" ")
-var amount = prms[0]
-var currency = prms[1]
-var address = prms[2]
-Bot.run({
-  command: "/onWithdraw",
-  options: { amount: amount, currency: currency, address: address }
-})
-if (amount == "false") {
-  return Bot.sendMessage("➡️* Send amount " + currency + "*")
+if (params) {
+  var prms = params.split(" ")
+  var amount = prms[0]
+  var currency = prms[1]
+  var address = prms[2]
+  Bot.run({
+    command: "/onWithdraw",
+    options: { amount: amount, currency: currency, address: address }
+  })
+  if (amount == "false") {
+    return Bot.sendMessage("➡️* Send amount " + currency + "*")
+  }
 }
